9dc68b430dd91c14f527ba6bc64a4df18985dc22
oussama <bz.oussama@gmail.com>
2020-01-31 15:37:49 +0100

    dummy data added

--- lib/screens/personel.dart
+++ lib/screens/personel.dart
@@ -13,7 +13,25 @@ class _PersonelScreenState extends State<PersonelScreen> {
         title: 'AAA Battreies',
         subtitle: 'Remote Controller',
         quantity: '2',
-        imageUrl: 'http://placekitten.com/50/50')
+        imageUrl:
+            'https://previews.123rf.com/images/koya79/koya791305/koya79130500020/19775806-aaa-battery-3d-icon.jpg'),
+    ShopItem(
+        title: 'Cat food',
+        subtitle: 'bring a fish meal if possible',
+        quantity: '1',
+        imageUrl: 'https://placekitten.com/408/287'),
+    ShopItem(
+        title: 'A4 Papers',
+        subtitle: '',
+        quantity: '1',
+        imageUrl:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXGZCt_2WbxVFGclJkj6T8lyNAKEmiaeFgzJHA6u_YczfNi9cy'),
+    ShopItem(
+        title: 'Paper rolls',
+        subtitle: '',
+        quantity: '2',
+        imageUrl:
+            'https://images-na.ssl-images-amazon.com/images/I/61%2BnMoPQbrL._AC_SL1500_.jpg')
   ];
 
   @override
