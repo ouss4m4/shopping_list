88d3e430c5d389bfba30b79386e707dd601f6f21
oussama <bz.oussama@gmail.com>
2020-01-31 15:29:08 +0100

    added slidable

--- lib/widgets/itemsList.dart
+++ lib/widgets/itemsList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/models/ShopItem.dart';
 import 'package:shopping_list/widgets/itemSingle.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ItemsList extends StatelessWidget {
   final List<ShopItem> shopItems;
@@ -8,10 +9,43 @@ class ItemsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    handleActions(ShopItem item, String action) {
+      print(item);
+      print(action);
+      if (action == 'Remove') {
+        final snackBar = SnackBar(content: Text('${item.title} removed'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      }
+    }
+
     return Container(
       child: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          return ItemSingle(shopItems[index]);
+          return Slidable(
+            actionPane: SlidableDrawerActionPane(),
+            actionExtentRatio: 0.25,
+            child: ItemSingle(shopItems[index]),
+            secondaryActions: <Widget>[
+              IconSlideAction(
+                caption: 'Remove',
+                color: Colors.red,
+                icon: Icons.delete_forever,
+                onTap: () => handleActions(shopItems[index], 'Remove'),
+              ),
+              IconSlideAction(
+                caption: 'Edit',
+                color: Colors.blue,
+                icon: Icons.edit,
+                onTap: () => handleActions(shopItems[index], 'Edit'),
+              ),
+              IconSlideAction(
+                caption: 'Complete',
+                color: Colors.green,
+                icon: Icons.check_circle,
+                onTap: () => handleActions(shopItems[index], 'Complete'),
+              ),
+            ],
+          );
         },
         itemCount: shopItems.length,
       ),
